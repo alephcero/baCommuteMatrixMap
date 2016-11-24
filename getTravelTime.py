2ca1b2cf6c5bc2d929500f9a956202d6b8241ccb
alephcero <4lephcero@gmail.com>
2016-11-24 13:40:30 -0500

    function and explanaitoin

--- getTravelTime.py
+++ getTravelTime.py
@@ -1,26 +1,13 @@
 #libraries
-import googlemaps
-import os
-import json
-try:
-    import urllib2 as urllib
-except ImportError:
-    import urllib.request as urllib
-
-
-#There should be a key stored as and enviromental variable
-key = os.getenv('GOOGLEAPIDISTANCE')
-gmaps = googlemaps.Client(key=os.getenv('GOOGLEAPIDISTANCE'))
-
-def getTravelTime(origins,destinations):
-'''
-This function takes a coordinates tuple for origin
-a coordinates tuple for destination
-and returns the travel time in seconds
-'''
-    travel = gmaps.distance_matrix(origins = home,
-    	destinations = work,
-    	mode="transit",
-        traffic_model="best_guess")
+def getTravelTime(origins,destinations,preferences = 'fewer_transfers'):
+    '''
+    This function takes a coordinates tuple for origin
+    a coordinates tuple for destination
+    and returns the travel time in seconds
+    preference could be fewer_transfers or less_walking
+    '''
+    key = os.getenv('GOOGLEAPIDISTANCE')
+    gmaps = googlemaps.Client(key=os.getenv('GOOGLEAPIDISTANCE'))
+    travel = gmaps.distance_matrix(origins = origins,destinations = destinations, mode="transit", traffic_model="best_guess", transit_routing_preference = preferences)
     return travel['rows'][0]['elements'][0]['duration']['value']
 
